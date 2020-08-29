50ec52dbb77711a9ccb86047cfe1aeb83c605c49
Ac_K <Acoustik666@gmail.com>
2020-08-29 03:04:33 +0200

    Restyle the homepage

--- www/main.js
+++ www/main.js
@@ -2,21 +2,24 @@ $(document).ready(function() {
     function encode(r){return r.replace(/[\x26\x0A\<>'"]/g,function(r){return"&#"+r.charCodeAt(0)+";"})}
 
     $.getJSON("/api", function(data) {
-        $(".main-stats").append('<div class="row"><div class="col-1"><i class="fas fa-user"></i></div><div class="col-6">Public Players</div><div class="col-4 text-right">' + data.public_players_count + '</div></div>');
-        $(".main-stats").append('<div class="row"><div class="col-1"><i class="fas fa-user-secret"></i></div><div class="col-6">Private Players</div><div class="col-4 text-right">' + data.private_players_count + '</div></div>');
-        $(".main-stats").append('<div class="row"><div class="col-1"><i class="fas fa-users"></i></div><div class="col-6">Total Players</div><div class="col-4 text-right">' + data.total_players_count + '</div></div>');
-        $(".main-stats").append('<div class="row row-space"></div>');
-        $(".main-stats").append('<div class="row"><div class="col-1"><i class="fas fa-gamepad"></i></div><div class="col-6">Public Games</div><div class="col-4 text-right">' + data.public_games_count + '</div></div>');
-        $(".main-stats").append('<div class="row"><div class="col-1"><i class="fas fa-lock"></i></div><div class="col-6">Private Games</div><div class="col-4 text-right">' + data.private_games_count + '</div></div>');
-        $(".main-stats").append('<div class="row"><div class="col-1"><i class="fas fa-layer-group"></i></div><div class="col-6">Total Games</div><div class="col-4 text-right">' + data.total_games_count + '</div></div>');
-        $(".main-stats").append('<div class="row row-space"></div>');
-        $(".main-stats").append('<div class="row"><div class="col-1"><i class="fas fa-people-arrows"></i></div><div class="col-6">In Connection</div><div class="col-4 text-right">' + data.in_progress_count + '</div></div>');
-        $(".main-stats").append('<div class="row"><div class="col-1"><i class="fas fa-server"></i></div><div class="col-6">On Proxy Server</div><div class="col-4 text-right">' + data.master_proxy_count + '</div></div>');
+        $(".players-public").text(data.public_players_count);
+        $(".players-private").text(data.private_players_count);
+        $(".players-total").text(data.total_players_count);
+
+        $(".games-public").text(data.public_games_count);
+        $(".games-private").text(data.private_games_count);
+        $(".games-total").text(data.total_games_count);
+
+        $(".in-progress-total").text(data.in_progress_count);
+        $(".proxy-server-total").text(data.master_proxy_count);
     });
 
     $.getJSON("/api/public_games", function(data) {
         $.each(data, function() {
-            $(".games-table").append('<tr><th><i class="fas fa-gamepad"></i> ' + this.game_name + '<br /><span class="titleid">(' + this.title_id + ')</span></th><td>' + this.player_count + '/' + this.max_player_count + ' <i class="fas fa-users"></i><br /><i class="fas fa-home"></i> ' + this.players.map(player => encode(player)).join(', <i class="fas fa-user"></i> ') + '</td><td><i class="fas fa-network-wired"></i> ' + this.mode + ' (' + this.status + ')</td></tr>');
+            $(".public-games").append('<div class="card margin-bt shadow"><div class="card-header"><div class="row"><div class="col-sm-10"><i class="red-sw fas fa-gamepad"></i> ' + this.game_name + ' <span class="badge badge-dark">' + this.title_id + '</span></div>'
+                                    + '<div class="col-sm-2 games-players-number"><i class="blue-sw fas fa-users"></i> ' + this.player_count + '/' + this.max_player_count + ' Players</div></div></div>'
+                                    + '<div class="card-body"><blockquote class="blockquote mb-0"><i class="blue-sw fas fa-home"></i> ' + this.players.map(player => encode(player)).join(', <i class="fas fa-user"></i> ')
+                                    + '<footer class="blockquote-footer"><i class="blue-sw fas fa-network-wired"></i> ' + this.mode + ' (' + this.status + ')</footer></blockquote></div></div>');
         });
     });
 });
