399644fa6bcebfc109a03ab4f27062e6dd4cb2c1
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-06-18 18:28:10 +0200

    Update nix files

--- default.nix
+++ default.nix
@@ -16,7 +16,7 @@ buildDotnetModule rec {
   dotnet-runtime = dotnetCorePackages.runtime_7_0;
   selfContainedBuild = false;
 
-  dotnetFlags = [  ];
+  dotnetFlags = [ "-p:PublishAOT=false" ];
 
   executables = [ "LanPlayServer" ];
 }
--- deps.nix
+++ deps.nix
@@ -1,3 +1,7 @@
 { fetchNuGet }: [
   (fetchNuGet { pname = "NetCoreServer"; version = "7.0.0"; sha256 = "0rhc03cm9rq5d0dl5m3m2ia68b4q5bf4vycv5f475dh1as7sh37w"; })
+  (fetchNuGet { pname = "NRedisStack"; version = "0.6.1"; sha256 = "0whfl2hlvhrppmpda0fakz2b20ka52xrjdfcp9a5ych6966d7h3j"; })
+  (fetchNuGet { pname = "Pipelines.Sockets.Unofficial"; version = "2.2.2"; sha256 = "1irxjvfhl4rm0xqypysfwiwwhaq9vp72560cn4i385yack0c6yrl"; })
+  (fetchNuGet { pname = "StackExchange.Redis"; version = "2.6.96"; sha256 = "0qa04l26vdjjpxn2infz0z6frkv9d4f9zjmmphwyfy1hj92lm665"; })
+  (fetchNuGet { pname = "System.IO.Pipelines"; version = "5.0.1"; sha256 = "1zvfcd2l1d5qxifsqd0cjyv57nr61a9ac2ca5jinyqmj32wgjd6v"; })
 ]
